bc872d32292e1a57bd31d77406220ca1f185ae66
ahmedkholady <ahmedkholady0@gmail.com>
2026-04-11 11:33:24 +0200

    update frontend + backend fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://el3bbas.vercel.app/',
+  baseURL: import.meta.env.VITE_API_URL || 'https://el3bbas.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
